962bd5954e2d8c7fc9399853f78a95bc4630e8dd
hatrickpansen <patrickhansen41@gmail.com>
2020-10-22 16:42:57 +0200

    node project structure fix

--- authServer/server.js
+++ authServer/server.js
@@ -7,10 +7,9 @@ const bodyParser = require('body-parser');
 
 const cookieParser = require('cookie-parser');
 
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 // Middlewares
@@ -28,12 +27,12 @@ const users = [
         username: 'Pat',
         password: 'hej'
     }
-]
+];
 
-const refreshTokens = []
+const refreshTokens = [];
 
-let secret = 'tihifnis'
-let refreshSecret = 'vrysecuresecret'
+let secret = 'tihifnis';
+let refreshSecret = 'vrysecuresecret';
 app.post('/auth/login', (req, res) => {
     let username = req.body.username;
     const user = users.find((user) => user.username === req.body.username);
@@ -45,14 +44,24 @@ app.post('/auth/login', (req, res) => {
     console.log(user);
 
     if (user.password == req.body.password) {
-        const accesstoken = generateAccessToken(user)
-        const refreshToken = generateRefreshToken(user)
-        refreshTokens.push(refreshToken)
-        var fiveMins = 5 * 60 * 1000
-        var oneWeek = 7 * 24 * 3600 * 1000
-        res.cookie('authcookie', accesstoken, {maxAge: fiveMins, httpOnly: false, secure: true, sameSite: "lax"})
-        res.cookie('refreshcookie', refreshToken, {maxAge: oneWeek, httpOnly: true, secure: true, sameSite: "lax" })
-        res.send()
+        const accesstoken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+        refreshTokens.push(refreshToken);
+        var fiveMins = 5 * 60 * 1000;
+        var oneWeek = 7 * 24 * 3600 * 1000;
+        res.cookie('authcookie', accesstoken, {
+            maxAge: fiveMins,
+            httpOnly: false,
+            secure: true,
+            sameSite: 'lax'
+        });
+        res.cookie('refreshcookie', refreshToken, {
+            maxAge: oneWeek,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'lax'
+        });
+        res.send();
     } else {
         console.log('Invalid password');
     }
@@ -77,27 +86,27 @@ function authenticateToken(req, res, next) {
 }
 
 function authenticateRefreshToken(req, res, next) {
-    const refreshCookie = req.cookies.refreshcookie
+    const refreshCookie = req.cookies.refreshcookie;
     if (refreshTokens.includes(refreshCookie)) {
         jwt.verify(refreshCookie, refreshSecret, (err, data) => {
             if (err) {
-                res.sendStatus(403)
+                res.sendStatus(403);
             } else if (data.user) {
-                req.user = data.user
+                req.user = data.user;
             }
-            next()
-        })
+            next();
+        });
     }
 }
 
 function generateAccessToken(user) {
-    return jwt.sign(user, secret, {expiresIn: '5m'})
+    return jwt.sign(user, secret, { expiresIn: '5m' });
 }
 
 function generateRefreshToken(user) {
-    return jwt.sign(user, refreshSecret, {expiresIn: '7d'})
+    return jwt.sign(user, refreshSecret, { expiresIn: '7d' });
 }
 
 app.get('/api', authenticateToken, (req, res) => {
-    console.log("Cookie checked, success")
-})
+    console.log('Cookie checked, success');
+});
