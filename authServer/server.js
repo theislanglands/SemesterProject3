ed326a9d5b78bdf4a782d7dd2cdadb16d8688a32
Ardit <sejeardit@hotmail.com>
2020-10-22 15:11:20 +0200

    refactor: Refactor for .env

--- authServer/server.js
+++ authServer/server.js
@@ -1,74 +1,80 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const path = require('path')
+const path = require('path');
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'))
-})
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 // Middlewares
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 //Listen
 app.listen(3300, () => {
-    console.log("Connected")
-})
+    console.log('Connected');
+});
 
 const users = [
     {
-        username: "Pat",
-        password: "hej"
+        username: 'Pat',
+        password: 'hej'
     }
-]
-let secret = 'tihifnis'
+];
+
+require('dotenv').config({ path: '../.env' });
+
 app.post('/auth/login', (req, res) => {
-    let username = req.body.username
-    const user = users.find(user => user.username == req.body.username)
-    
+    let username = req.body.username;
+    const user = users.find((user) => user.username === req.body.username);
+
     if (user == null) {
-        return res.status(400).send(`Cannot find user with username: ${username}`)
+        return res.status(400).send(`Cannot find user with username: ${username}`);
     }
 
-    console.log(user)
+    console.log(user);
 
-    if (user.password == req.body.password) {
-        const token = jwt.sign({user: username}, secret, {expiresIn: '5m'})
+    if (user.password === req.body.password) {
+        const token = jwt.sign({ user: username }, process.env.SECRET, { expiresIn: '5m' });
         var fiveMins = 5 * 60 * 1000;
-        res.cookie('authcookie', token, {maxAge: fiveMins, httpOnly: true, secure: false, sameSite: "lax"}).send()
+        res.cookie('authcookie', token, {
+            maxAge: fiveMins,
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax'
+        }).send();
     } else {
-        console.log("Invalid password")
+        console.log('Invalid password');
     }
-})
+});
 
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname + '/home.html'))
-})
+    res.sendFile(path.join(__dirname + '/home.html'));
+});
 
 function authenticateToken(req, res, next) {
-    const authcookie = req.cookies.authcookie
-    console.log(authcookie)
-    console.log(req.cookies)
-    jwt.verify(authcookie, secret, (err, data) => {
+    const authcookie = req.cookies.authcookie;
+    console.log(authcookie);
+    console.log(req.cookies);
+    jwt.verify(authcookie, process.env.SECRET, (err, data) => {
         if (err) {
-            res.sendStatus(403)
+            res.sendStatus(403);
+        } else if (data.user) {
+            req.user = data.user;
+            next();
         }
-        else if (data.user) {
-            req.user = data.user
-            next()
-        }
-    })
+    });
 }
 
 app.get('/api', authenticateToken, (req, res) => {
-    console.log("Cookie checked, success")
-    console.log(req.cookies.authcookie)
-})
+    console.log('Cookie checked, success');
+    console.log(req.cookies.authcookie);
+});
