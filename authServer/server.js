46a50d89826e374f7cbbc74d13ac5f82505acfd3
hatrickpansen <patrickhansen41@gmail.com>
2020-10-22 15:56:42 +0200

    code cleanup

--- authServer/server.js
+++ authServer/server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 const jwt = require('jsonwebtoken')
+const { send } = require('process')
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
@@ -29,7 +30,11 @@ const users = [
         password: "hej"
     }
 ]
+
+const refreshTokens = []
+
 let secret = 'tihifnis'
+let refreshSecret = 'vrysecuresecret'
 app.post('/auth/login', (req, res) => {
     let username = req.body.username
     const user = users.find(user => user.username == req.body.username)
@@ -41,9 +46,14 @@ app.post('/auth/login', (req, res) => {
     console.log(user)
 
     if (user.password == req.body.password) {
-        const token = jwt.sign({user: username}, secret, {expiresIn: '5m'})
-        var fiveMins = 5 * 60 * 1000;
-        res.cookie('authcookie', token, {maxAge: fiveMins, httpOnly: true, secure: false, sameSite: "lax"}).send()
+        const accesstoken = generateAccessToken(user)
+        const refreshToken = generateRefreshToken(user)
+        refreshTokens.push(refreshToken)
+        var fiveMins = 5 * 60 * 1000
+        var oneWeek = 7 * 24 * 3600 * 1000
+        res.cookie('authcookie', accesstoken, {maxAge: fiveMins, httpOnly: false, secure: true, sameSite: "lax"})
+        res.cookie('refreshcookie', refreshToken, {maxAge: oneWeek, httpOnly: true, secure: true, sameSite: "lax" })
+        res.send()
     } else {
         console.log("Invalid password")
     }
@@ -68,7 +78,28 @@ function authenticateToken(req, res, next) {
     })
 }
 
+function authenticateRefreshToken(req, res, next) {
+    const refreshCookie = req.cookies.refreshcookie
+    if (refreshTokens.includes(refreshCookie)) {
+        jwt.verify(refreshCookie, refreshSecret, (err, data) => {
+            if (err) {
+                res.sendStatus(403)
+            } else if (data.user) {
+                req.user = data.user
+            }
+            next()
+        })
+    }
+}
+
+function generateAccessToken(user) {
+    return jwt.sign(user, secret, {expiresIn: '5m'})
+}
+
+function generateRefreshToken(user) {
+    return jwt.sign(user, refreshSecret, {expiresIn: '7d'})
+}
+
 app.get('/api', authenticateToken, (req, res) => {
     console.log("Cookie checked, success")
-    console.log(req.cookies.authcookie)
 })
