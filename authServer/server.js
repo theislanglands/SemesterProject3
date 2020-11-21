decbefdea8e8780bb03b1b41000a0e5f20e7a5f1
lucas sass <lucassass@live.com>
2020-11-21 13:08:11 +0100

    fix: change to href route

--- authServer/server.js
+++ authServer/server.js
@@ -57,7 +57,7 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname + '/html/login-page.html'));
 });
 
-// test 2 
+// test 2
 //atempt to fix authentication on /music by inserting authenticateRefreshToken
 app.get('/music', authenticateRefreshToken, (req, res) => {
     res.sendFile(path.join(__dirname + '/html/music.html'));
