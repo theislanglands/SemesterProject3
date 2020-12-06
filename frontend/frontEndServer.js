a3978e4b103f85618119986996c8e4eb5eb6e5af
markus munk <mmunk19@student.sdu.dk>
2020-12-06 17:16:18 +0100

    frontend files and dockerfile

---
+++ frontend/frontEndServer.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const server = express();
+const validator = require('validator');
+
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html');
+});
+server.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/html/login/login-page.html');
+});
+server.get('/forgotPass', (req, res) => {
+    res.sendFile(__dirname + '/html/forgot/forgotPasword-page.html');
+});
+server.get('/forgotPass.js', (req, res) => {
+    res.sendFile(__dirname + '/html/forgot/forgotPass.js');
+});
+
+server.get('/reset', (req, res) => {
+    res.sendFile(__dirname + '/html/forgot/resetPassword.html');
+});
+server.get('/resetPassFront.js', (req, res) => {
+    res.sendFile(__dirname + 'html/forgot/resetPassFront.js');
+});
+server.listen(8090);
---
+++ frontend/html/forgot/forgotPass.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', (event) => {
+    var button = document.getElementById('submit');
+    var div = document.getElementById('response');
+    var msg = '';
+    button.addEventListener('click', function () {
+        var input = document.getElementById('email');
+        let response = fetch('https://localhost/gmail/forgotPass', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Accept: 'application/json'
+            },
+            body: 'email=' + encodeURIComponent(input.value)
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                div.innerText = data.msg;
+
+                input.value = '';
+                input.disabled = data.isSent;
+            })
+            .catch((error) => {
+                console.log('some error happened');
+            });
+        console.log(msg);
+    });
+});
