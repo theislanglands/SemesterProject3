55d9d6c29ee9b64e24be58e4a395859f522997f9
lucassass <lucassass@live.com>
2020-12-15 10:35:24 +0100

    chore env changed

--- authServer/server.js
+++ authServer/server.js
@@ -37,10 +37,10 @@ app.use(function (req, res, next) {
 let refreshSecret = fs.readFileSync('./refreshSecret.key', 'utf8');
 //subscription db URL
 // eslint-disable-next-line no-undef
-const subscriptionURL = process.env.SUBSCRIPTION_URL_MOCK;
+const subscriptionURL = process.env.SUBSCRIPTION_URL;
 //connetionSecurity URL
 // eslint-disable-next-line no-undef
-const dataSecurityURL = process.env.DATASECURITY_URL_MOCK;
+const dataSecurityURL = process.env.DATASECURITY_URL;
 //own service URL
 // eslint-disable-next-line no-undef
 //eslint-disable-next-line no-unused-vars
