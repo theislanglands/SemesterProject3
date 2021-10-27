c90a050a36d42b967473af4bbd5e9ef5acec988d
jobau19 <jobau19>
2021-10-27 14:14:03 +0200

    template for get_json & get_mp3

--- MediaAcquisition/youtubeDLManager/ytdlcontroller.py
+++ MediaAcquisition/youtubeDLManager/ytdlcontroller.py
@@ -1,7 +1,5 @@
 import yt_dlp
 
-
-
 ydl_opts = {
     'format': 'bestaudio/best',
     'writeinfojson': True,
@@ -12,5 +10,34 @@ ydl_opts = {
         'preferredquality': '192',
 
     }], }
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=kffacxfA7G4'])
+
+
+def ytDL(url):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+
+if __name__ == '__main__':
+
+
+    url = input()
+    ytDL(url)
+
+
+class YoutubeDL():
+
+    def __init__(self, url):
+            self.url = url
+
+
+    def get_json(self):
+        #jsondata = YDL.EXTRACTINFO(url)
+        #
+
+        pass
+
+    def get_mp3(self):
+        pass
+
+
