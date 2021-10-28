4e59c8f58135666686fba042f8e02225bd7e8515
jobau19 <jobau19>
2021-10-28 12:49:08 +0200

    changed savepath, savedfilename etc.

--- MediaAcquisition/youtubeDLManager/ytdlcontroller.py
+++ MediaAcquisition/youtubeDLManager/ytdlcontroller.py
@@ -1,43 +1,34 @@
 import yt_dlp
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'writeinfojson': True,
-    'clean_infojson': True,
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
 
-    }], }
+class YoutubeDL:
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'writeinfojson': True,
+        'clean_infojson': True,
+        'outtmpl': '/home/baumann/PycharmProjects/media-acquisition/MediaAcquisition/youtubeDLManager/temp/YT_%(id)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }], }
 
+    def init(self):
+        pass
 
-def ytDL(url):
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    def get_json(self, url):
+        with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+            json_data = ydl.extract_info(url)
+            return json_data
 
+    def get_mp3(self, url):
+        # Download mp3
+        # send to server
+        # if succes. return TRue, 200 'ok'
+        pass
 
 
 if __name__ == '__main__':
-
-
-    url = input()
-    ytDL(url)
-
-
-class YoutubeDL():
-
-    def __init__(self, url):
-            self.url = url
-
-
-    def get_json(self):
-        #jsondata = YDL.EXTRACTINFO(url)
-        #
-
-        pass
-
-    def get_mp3(self):
-        pass
-
-
+    y = YoutubeDL()
+    data = y.get_json('dQw4w9WgXcQ')
+    print(data['id'])
