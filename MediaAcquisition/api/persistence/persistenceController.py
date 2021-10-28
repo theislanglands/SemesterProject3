9a2e8498478fbcc5b2e4ae8aac7c47ebeb9e4468
Theis Langlands <thlan20@student.sdu.dk>
2021-10-28 12:55:43 +0200

    lavet try-catch på store

--- MediaAcquisition/api/persistence/persistenceController.py
+++ MediaAcquisition/api/persistence/persistenceController.py
@@ -1,6 +1,7 @@
+import ftplib
 from ftplib import FTP
 
-    #ftp config
+# ftp config
 host = 'ftp.foreignlands.dk'
 domain = 'foreignlands.dk'
 root = 'MediaAcquisition'
@@ -10,30 +11,34 @@ pw = 'MediaAcquisition09!'
 
 ftp = FTP(host)  # connect to host, default port (21)
 ftp.login(username, pw)
-print(ftp.getwelcome()) # check connection
-ftp.cwd(root) # change workingdirectory
-ftp.dir() # show contents of folder
-id ="all_my_love"
+print(ftp.getwelcome())  # check connection
+ftp.cwd(root)  # change workingdirectory
+ftp.dir()  # show contents of folder
+id = "all_my_love"
 
 
 def storeAudio(localFilePath, fileNameOnServer):
+    try:
+        file = open(localFilePath, "rb")  # open file to send
+        ftp.storbinary('STOR ' + '/' + root + '/' + fileNameOnServer, file)  # send the file
+        return True
 
-    file = open(localFilePath, "rb")  # file to send
-
-    ftp.storbinary('STOR '+ '/' + root + '/'+fileNameOnServer, file)  # send the file
-
-    file.close()
-    #return boolean
+    except ftplib.all_errors as e:
+        print('Ftp fail -> ', e)
+        return False
 
 def check_id(YoutubeID):
     pass
 
+
 def checkIfExist(id):
     pass
+
 
 def getAudio(id):
     returnURL = 'http://' + domain + '/' + root + '/' + id + '.mp3'
     return returnURL
+
 
 def downloadAudio(id, path):
     filename = id + '.mp3'
@@ -41,7 +46,6 @@ def downloadAudio(id, path):
 
 
 filepath = '/Users/theislanglands/Dropbox/SDU/Semester3/semesterprojekt/media-acquisition/MediaAcquisition/api/persistence/test-upload.txt'
-#storeAudio(filepath, 'test-upload.txt')
+# storeAudio(filepath, 'test-upload.txt')
 
 print(getAudio("all_my_love"))
-
