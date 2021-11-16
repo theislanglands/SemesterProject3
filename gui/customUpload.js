3260070fef0524a19b158ed8677b113e6ec57f9c
Theis Langlands <thlan20@student.sdu.dk>
2021-11-16 17:07:22 +0100

    Changed custum upload Gui
    Added javascript funtionality to show / hide collection fields

--- gui/customUpload.js
+++ gui/customUpload.js
@@ -10,6 +10,13 @@ window.onload = () => {
     addSizeListener("audio", scaleBytes(maxAudioSize, 3));
 }
 
+window.addEventListener("load", e=> {
+    document.getElementById('is_collection').addEventListener('click', e => {
+        showCollectionFields();
+    });
+});
+
+
 function scaleBytes(x, scale) {
     for (var i = 0; i < scale; i++) {
         x = x * 1024; // scale a clean integer MB to its value in bytes
@@ -23,6 +30,18 @@ function addSizeListener(id, size) {
         if (this.files[0].size > size) { //get size of file, and check if its greater than maximum size
             alert("File is too big for " + id + "!"); //alert user
             this.value = ""; //reset value fo element
+        }
+    }
+}
+
+function showCollectionFields(){
+    let elements = document.getElementsByClassName("col");
+
+    for (let i=0; i<elements.length; i++){
+        if (document.getElementById("is_collection").checked){
+            elements[i].disabled = false;
+        } else {
+            elements[i].disabled = true;
         }
     }
 }
@@ -47,11 +66,11 @@ function cuSubmit() {
     }
 
     // Retrieve values from form
-    metaDataJson['name'] = document.getElementById("name").value
-    metaDataJson['artist'] = document.getElementById("artist").value
+    metaDataJson['name'] = document.getElementById("name").value;
+    metaDataJson['artist'] = document.getElementById("artist").value;
     metaDataJson['release_year'] = document.getElementById("release_year").value;
-    metaDataJson['part of collection'] = document.getElementById("is_collection").value
-    metaDataJson['collection_name'] = document.getElementById("collection_name").value
+    metaDataJson['part of collection'] = document.getElementById("is_collection").checked;
+    metaDataJson['collection_name'] = document.getElementById("collection_name").value;
     metaDataJson['track_nr'] = document.getElementById("track_nr").value;
     metaDataJson['total_track_count'] = document.getElementById("total_track_count").value;
     let jsonFile = JSON.stringify(metaDataJson)
@@ -59,6 +78,8 @@ function cuSubmit() {
 
 /*
 //send data to api
+
+// check up 'JQUERY!
 
     httpRequest.open('POST', apiEndpoint, true); //open request to api
 
@@ -73,6 +94,17 @@ function cuSubmit() {
 
     //add 2 files (art and audio)
     httpRequest.send(metaDataJson, audioFile, artFile);
+$.ajax({
+            method: "post",
+            url: urllink,
 
+            success: function (result) {
+                $('body').append(result)
+            },
+
+            error: function () {
+                alert("error something went wrong");
+            }
+        });
  */
 }
