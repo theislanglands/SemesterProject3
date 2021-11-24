aba712a321df5461afc503ca2771efd23a985fae
jobau19 <Jrb130490#1043>
2021-11-24 13:13:54 +0100

    fixed database

--- MediaAcquisition/MediaAcquisition/settings.py
+++ MediaAcquisition/MediaAcquisition/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': 'media_acquisition',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'HOST': 'database',
         'PORT': '3306',
     }
 }
