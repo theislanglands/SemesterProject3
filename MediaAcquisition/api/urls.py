c2f605ccad6b4d3d58711a4ee2c669a4159506fb
Thomas <Raithdk@gmail.com>
2021-12-01 15:33:14 +0100

    added URL names

--- MediaAcquisition/api/urls.py
+++ MediaAcquisition/api/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('dummy/<link>', api_call),
-    path('add_youtube/<link>', add_youtube_audio),
-    path('delete_track/<link>', delete_audio),
-    path('get_track/<link>', get_audio),
-    path('get_all_tracks/', get_all_tracks),
-    path('get_metadata/<link>', get_metadata),
-    path('add_custom', upload_file)
+    path('dummy/<link>', api_call, name='dummy'),
+    path('add_youtube/<link>', add_youtube_audio, name='add_yt'),
+    path('delete_track/<link>', delete_audio, name='delete_track'),
+    path('get_track/<link>', get_audio, name='get_track'),
+    path('get_all_tracks/', get_all_tracks, name='get_all_tracks'),
+    path('get_metadata/<link>', get_metadata, name='get_metadata'),
+    path('add_custom', upload_file, name='add_custom')
 ]
 
